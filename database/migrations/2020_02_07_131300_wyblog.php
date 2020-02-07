accdd050410799a2c0985d55501ed29cb8ea1655
wuye251 <754093136@qq.com>
2020-02-07 21:43:47 +0800

    文章blog表创建  通过迁移生成

---
+++ database/migrations/2020_02_07_131300_wyblog.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Wyblog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblBlog', function (Blueprint $table) {
+            $table->bigIncrements('id')
+                  ->comment('主键ID');
+            $table->string('title')
+                  ->default('')
+                  ->comment('标题');
+            $table->string('content')
+                  ->default('')
+                  ->comment('文章内容');
+            $table->tinyInteger('status')
+                  ->default(0)
+                  ->comment('文章状态 0未发布 1已发布 2已删除');
+            $table->timestamp('create_time')
+                  // ->default(0)
+                  ->comment('创建时间');
+            $table->timestamp('update_time')
+                  // ->default(0)
+                  ->comment('修改时间');
+            $table->string('author')
+                  ->default('')
+                  ->comment('作者');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tblBlog');
+    }
+}
