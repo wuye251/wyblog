41271f54c500d563fc3d3ef5e1f73c15d0b67c0c
wuye251 <754093136@qq.com>
2020-02-20 21:14:16 +0800

    首页展示blogs后端逻辑完成

--- resources/views/blog/showAllBlog.blade.php
+++ resources/views/blog/showAllBlog.blade.php
@@ -1,2 +1,36 @@
 <!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>wyblogs查看</title>
+<meta name="viewpoint" content="width=device-width, initial-sacle=1">
 
+<style>
+	body {
+		margin: 0;
+	}
+	.header {
+		background-color: #f1f1f1;
+  		padding: 1;
+  		text-align: center;
+	}
+	.title {
+		background-color: #000000;
+		padding: 1;
+		text-align: center;
+	}
+</style>
+</head>
+
+<body>
+
+	<div class="header">
+		<h1>头部区域</h1>
+	</div>
+
+	<div class="title">
+		<h2>横栏内容</h2>
+	</div>
+</body>
+</head>
+</html>
