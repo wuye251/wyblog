4589d7aadb93a07d337762157af05d66b4e292d0
wuye251 <754093136@qq.com>
2020-02-21 22:47:43 +0800

    首页布局完成

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,103 +2,55 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>wyblogs查看</title>
-<!-- <meta name="viewpoint" content="width=device-width, initial-sacle=1"> -->
-<!-- <base href="wyblog/blog/"> -->
-
-<style>
-	body {
-		margin: 0 0 0 0;
-	}
-
-	.header {
-		background-color: #000000;
-		padding: 1;
-		height: 65px;
-		text-align: center;
-	}
-	.search {
-		width: 100px;
-		height: 30px;
-		margin-top: 5px;
-		margin: 6px 0 50px 0; 
-	}
-
-	.title {
-		background-color: #f1f1f1;
-  		padding: 1;
-  		text-align: center;
-  		/*display: block;*/
-	}
-	.titleText {
-		font-size: 100%;
-		text-align: center;
-	}
-	/*初始颜色*/
-	a:visited {color:white;}
-	/*鼠标悬浮*/
-	a:hover {color:green;}
-
-	.sideBar {
-		width: 180px;
-		height: 200px;
-		padding-top: 200px;
-		padding-left:200px;
-		/*边框*/
-		border-style: solid;
-		/*和内容框并排*/
-		float: left;
-	}
-	.Maincontent {
-		width:500px;
-		height: 500px;
-		border-style: solid;
-		padding-top: 200px;
-		padding-left: 200px;
-		float: left;
-	}
-	.rightSideBar {
-		width: 240px;
-		height: 500px;
-		/*padding-top: 30px;*/
-		/*padding-left:30px;*/
-		/*边框*/
-		border-style: solid;
-		/*内容右侧悬浮*/
-		float: left;
-	}
-
-</style>
+<title>div布局</title>
+	<style type="text/css">
+		body {
+			margin: 0;
+		}
+		#container {
+			width: 100%;
+			height: 950px;
+			background-color: darkgray;
+		}
+		#heading {
+			width: 100%;
+			height: 5%;
+			background-color: black;
+			color : white;
+		}
+		#content_menu {
+			width: 25%;
+			height: 40%;
+			background-color: green;
+			float: left;
+		}		
+		#content_body {
+			width: 60%;
+			height: 90%;
+			background-color: yellow;
+			float: left;
+		}
+		#content_sidebar {
+			width: 15%;
+			height: 70%;
+			background-color: pink;
+			float: left;
+		}
+		#footing {
+			width:100%;
+			height: 5%;
+			background-color: blue;
+			clear: both;
+		}
+	</style>
 </head>
-
 <body>
-
-	<div class="header">
-		<!-- 搜索栏 -->
-		<input class="search" type="text" name="searchContent">
-
-		<a class="titleText" href="showAll">博客</a>s
-	</div>
-
-	<div class="title">
-		<span><a href="">横栏内容</a></span>
-	</div>
-
-	<div class="content">
-		<div class="sideBar">
-			侧边栏
-		</div>
-		
-		<div class="Maincontent">
-			内容栏
-		</div>
-		
-		<div class="rightSideBar">
-			右侧边栏
-		</div>
+	<div id="container">
+		<div id="heading">头部</div>
+		<div id="content_menu">左侧用户信息</div>
+		<div id="content_body">内容主体</div>
+		<div id="content_sidebar">右侧栏</div>
+		<div id="footing">底部</div>	
 	</div>
 </body>
-
-
-</head>
 </html>
