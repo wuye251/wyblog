fc174eb36b82aad5b91c56eb0d354a64635072c5
吴烨 <wuye@bszhihui.com>
2022-06-16 16:21:39 +0800

    用户列表

--- web/admin/src/api/user.js
+++ web/admin/src/api/user.js
@@ -11,8 +11,8 @@ export function login(data) {
 
 export function userList(data) {
   return request({
-    url: '/api/v1/list',
+    url: '/api/v1/user/list',
     method: 'get',
-    data:data
+    params:data
   })
 }
--- web/admin/src/components/user/List.vue
+++ web/admin/src/components/user/List.vue
@@ -14,43 +14,116 @@
                 </a-button>
             </a-col>
         </a-row>
-        <a-table
-            :columns="columns"
-            :row-key="record => record.login.uuid"
-            :data-source="dataSource"
+        <a-table 
+            :columns="columns" 
+            :data-source="data" 
+            @change="onChange"
+            :row-key="username"
             :pagination="pagination"
-            :loading="loading"
-            :dataSource="userList"
-            @change="handleTableChange"
-        >
-            <template #bodyCell="{ column, text }">
-            <template v-if="column.dataIndex === 'name'">{{ text.first }} {{ text.last }}</template>
-            </template>
-        </a-table>
+            bordered
+         />
     </a-card>
 
 </template>
 
 <script>
-import { defineComponent, reactive } from 'vue'
-import { userList } from '@/api/user.js'
-const columns = []
+import { defineComponent, reactive, computed,ref } from 'vue'
+import { userList } from '@/api/user.js' 
+import day from 'dayjs'
+const columns = [
+    {
+        title: 'ID',
+        dataIndex: 'ID',
+        width:'10%',
+        align: 'center',
+    },
+    {
+        title: '用户名',
+        dataIndex: 'username',
+        width:'10%',
+        align: 'center',
+    },
+    {
+        title: '身份',
+        dataIndex: 'role',
+        width:'10%',
+        align: 'center',
+    },
+    {
+        title: '创建时间',
+        dataIndex: 'CreatedAt',
+        width:'10%',
+        key: 'CreatedAt',
+        customRender: (val) => {
+            return val ? day(val.CreatedAt).format('YYYY/MM/DD HH:mm') : '暂无'
+        },
+        align: 'center',
+    },
+    {
+        title: '更新时间',
+        dataIndex: 'UpdatedAt',
+        width:'10%',
+        customRender: (val) => {
+            return val ? day(val.UpdatedAt).format('YYYY/MM/DD HH:mm') : '暂无'
+        },
+        align: 'center',
+    },
+    {
+        title: '操作',
+        align: 'center',
+    },
+
+]
 export default defineComponent({
+
     data() {
         return {
-            userList: [],
-            columns
+            columns,
+            data:[],
+            pagination: {  
+                defaultCurrent: 1,  // 默认当前页数  
+                defaultPageSize: 5,   // 默认当前页显示数据的大小  
+                total: 0, // 总数，必须先有  
+                showSizeChanger: true,  
+                showQuickJumper: true,  
+                pageSizeOptions: ['5','10', '20', '30'],  
+                showTotal: total => `共 ${total} 条`, // 显示总数  
+                onShowSizeChange: (current, pageSize) => {  
+                    this.pagination.defaultCurrent = 1  
+                    this.pagination.defaultPageSize = pageSize  
+                    this.getUserList()  //显示列表的接口名称  
+                },  
+                // 改变每页数量时更新显示  
+                onChange: (current, size) => {  
+                    this.pagination.defaultCurrent = current  
+                    this.pagination.defaultPageSize = size  
+                    this.getUserList()  
+                } // 点击页码事件  
+            } 
         }
     },
-    create() {
-        getUserList()
+ 
+    created() {
+        this.getUserList()
+        console.log('created---', this.data)
+
     },
     methods: {
-        getUserList() {
-            userList(columns).then(res => {
-
-            })
-        }
-    },
+        async getUserList() {
+            const { defaultCurrent, defaultPageSize } = this.pagination
+            //组装参数
+            const param = {  
+                pageSize: defaultPageSize,  
+                pageNum: defaultCurrent,
+            }
+            let res = await userList(param);
+            console.log(res,'res')
+            const total = res.total
+            const records = res.data
+            this.pagination.total = total  
+            this.data = res.data
+            console.log('this.data',this.data)
+        },
+    }
 })
 </script>
