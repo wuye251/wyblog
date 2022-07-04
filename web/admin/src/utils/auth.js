d75546395339a09dc337ee5ed55493b3f7ec716d
wuye251 <754093136@qq.com>
2022-07-04 21:31:13 +0800

    登录失效时长修改

--- web/admin/src/utils/auth.js
+++ web/admin/src/utils/auth.js
@@ -2,18 +2,20 @@ import { timelineItemProps } from 'ant-design-vue/es/timeline'
 import Cookies from 'js-cookie'
 
 const TokenKey = 'WYBLOG-TOKEN'
-const LiveTime = 10*24*60*60
+let hour = 10 //失效时间是几小时
+
 
 export function getToken() {
   return Cookies.get(TokenKey)
 }
 
 export function setToken(token) {
-  let seconds = LiveTime
-  let expires = new Date(new Date() * 1 + seconds * 1000)
+  let expires= new Date(new Date().getTime() + hour * 60 * 60 * 1000);
   return Cookies.set(TokenKey, token, {expires: expires})
 }
 
 export function removeToken() {
   return Cookies.remove(TokenKey)
 }
+
+
