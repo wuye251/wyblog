3883db58aa4af6d5501268845bbd1ee228a126cf
wuye251 <754093136@qq.com>
2023-05-13 10:37:27 +0800

    fix 代码高亮

--- web/front/src/main.js
+++ web/front/src/main.js
@@ -8,10 +8,22 @@ import VMdPreview from '@kangc/v-md-editor/lib/preview';
 import '@kangc/v-md-editor/lib/style/preview.css';
 import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js';
 import '@kangc/v-md-editor/lib/theme/style/vuepress.css';
-import Prism from 'prismjs';
+import creatPrismTheme from '@kangc/v-md-editor/lib/theme/prism';
+// import Prism from "prismjs"
 
+import Prism  from './prism';
+import './prism.css'
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-vim';
+
+// const prismTheme = creatPrismTheme({
+    // Prism,
+//     myPrism,
+// });
 VMdPreview.use(vuepressTheme, {
     Prism,
-});
+    // myPrism
+})
+// .vMdParser.theme(prismTheme);
 
 createApp(App).use(router).use(Antd).use(VMdPreview).mount('#app')
